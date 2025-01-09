12d6b4b1614ef3b12757830635dfc8601b34aeba
LastProgram <145275888+LastProgram@users.noreply.github.com>
2025-01-10 03:17:09 +0700

    Output notifications for user

--- app/src/main/java/com/example/alab7/PollWorker.kt
+++ app/src/main/java/com/example/alab7/PollWorker.kt
@@ -1,13 +1,16 @@
 package com.example.alab7
 
+import android.app.PendingIntent
 import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 private const val TAG = "PollWorker"
 
-class PollWorker(val context: Context, workerParams: WorkerParameters): Worker(context, workerParams) {
+class PollWorker(val context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
     override fun doWork(): Result {
         val query = QueryPreferences.getStoredQuery(context)
         val lastResultId = QueryPreferences.getLastResultId(context)
@@ -16,15 +19,14 @@ class PollWorker(val context: Context, workerParams: WorkerParameters): Worker(c
                 .execute()
                 .body()
                 ?.photos
-                ?.galleryItems
+                ?.galleryItems ?: emptyList()
         } else {
-            FlickrFetchr().searchPhotosRequest(
-                query)
+            FlickrFetchr().searchPhotosRequest(query)
                 .execute()
                 .body()
                 ?.photos
-                ?.galleryItems
-        } ?: emptyList()
+                ?.galleryItems ?: emptyList()
+        }
 
         if (items.isEmpty()) {
             return Result.success()
@@ -37,6 +39,32 @@ class PollWorker(val context: Context, workerParams: WorkerParameters): Worker(c
         } else {
             Log.i(TAG, "Got a new result: $resultId")
             QueryPreferences.setLastResultId(context, resultId)
+
+            val intent = PhotoGalleryActivity.newIntent(context)
+            val pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE // Добавлены флаги мутабельности
+            )
+            val resources = context.resources
+
+            // Проверка разрешений на отправку уведомлений
+            if (NotificationManagerCompat.from(context).areNotificationsEnabled()) { // Проверка позволяет избежать исключений
+                val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .build()
+
+                val notificationManager = NotificationManagerCompat.from(context)
+                notificationManager.notify(0, notification)
+            } else {
+                Log.i(TAG, "Notification permission not granted")
+            }
         }
 
         return Result.success()
