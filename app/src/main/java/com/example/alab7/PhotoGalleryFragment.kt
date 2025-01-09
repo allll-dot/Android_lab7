92703ddf7db2d3bd4a49f86c15c15631800d315d
LastProgram <145275888+LastProgram@users.noreply.github.com>
2025-01-10 00:52:58 +0700

    Adding thumbnail

--- app/src/main/java/com/example/alab7/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/alab7/PhotoGalleryFragment.kt
@@ -27,6 +27,16 @@ private const val TAG = "PhotoGalleryFragment"
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoGalleryViewModel : PhotoGalleryViewModel
     private lateinit var photoRecyclerView : RecyclerView
+    private lateinit var thumbnailDownloader : ThumbnailDownloader<PhotoHolder>
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+        thumbnailDownloader = ThumbnailDownloader()
+        lifecycle.addObserver(thumbnailDownloader)
+        photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -46,14 +56,10 @@ class PhotoGalleryFragment : Fragment() {
             })
     }
 
-    override fun onCreate(savedInstanceState : Bundle?) {
-        super.onCreate(savedInstanceState)
-        photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
-    }
-
     private class PhotoHolder(private val itemImageView: ImageView):
         RecyclerView.ViewHolder(itemImageView) {
         val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
+
     }
 
     private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>) : RecyclerView.Adapter<PhotoHolder>() {
@@ -68,7 +74,9 @@ class PhotoGalleryFragment : Fragment() {
             ) as ImageView
             return PhotoHolder(view)
         }
+
         override fun getItemCount(): Int = galleryItems.size
+
         override fun onBindViewHolder(holder : PhotoHolder, position: Int) {
             val galleryItem = galleryItems[position]
             val placeholder: Drawable = ContextCompat.getDrawable(
@@ -76,6 +84,7 @@ class PhotoGalleryFragment : Fragment() {
                 R.drawable.bill_up_close
             ) ?: ColorDrawable()
             holder.bindDrawable(placeholder)
+            thumbnailDownloader.queueThumbnail(holder, galleryItem.url)
         }
     }
 
