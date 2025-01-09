1bd06f598831a0c9b50124c8b8fcc7e80aae3c9d
LastProgram <145275888+LastProgram@users.noreply.github.com>
2025-01-09 22:19:35 +0700

    View data

---
+++ app/src/main/java/com/example/alab7/PhotoGalleryViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.alab7
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class PhotoGalleryViewModel : ViewModel() {
+    val galleryItemLiveData : LiveData<List<GalleryItem>>
+    init {
+        galleryItemLiveData = FlickrFetchr().fetchPhotos()
+    }
+}
